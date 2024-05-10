1f0fe0d6046d09a8be210515a95f1b1ce417061c
Fadia Khaled <fadiakhaledd2@gmail.com>
2024-05-10 12:48:54 +0300

    Time: 574 ms (21.83%) | Memory: 123.2 MB (6.11%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -1,32 +1,26 @@
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        List<int[]> fractions = getFractions(arr); 
-        List<Pair<int[], Float>> fractionValues = new ArrayList<>();
-        for (int[] fraction : fractions) {
-            float numerator = fraction[0];
-            float denominator = fraction[1];
-            float result = numerator / denominator;
-            fractionValues.add(
-                new Pair<int[], Float>(new int[]{fraction[0], fraction[1]}, result) );
-        }
-
+        
+        List<Pair<int[], Float>> fractionValues = getFractions(arr);
         Collections.sort(fractionValues, Comparator.comparing(Pair::getValue));
-
-        Pair<int[], Float> ans = fractionValues.get(k-1);
-
-        return ans.getKey();
+        return fractionValues.get(k-1).getKey();
     }
 
-    public List<int[]> getFractions (int [] arr) {
+    public  List<Pair<int[], Float>> getFractions (int [] arr) {
         int n = arr.length; 
-        List<int[]> fractions = new ArrayList<int[]>(); 
+        List<Pair<int[], Float>> fractionValues = new ArrayList<>();
+
         for (int i = 0; i < n-1; i++) {
             int j = i + 1; 
+            float numerator = arr[i]; 
             while (j < n) {
-                fractions.add(new int[]{arr[i], arr[j]});
+                float denominator = arr[j];
+                float result = (float) numerator / denominator;;
+                fractionValues.add(
+                new Pair<int[], Float>(new int[]{arr[i], arr[j]}, result));
                 j++; 
             }
         }
-        return fractions; 
+        return fractionValues; 
     }
 }
