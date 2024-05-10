5aa389951cced6de2ca7f5939b1379c0f1230f9a
Fadia Khaled <fadiakhaledd2@gmail.com>
2024-05-10 19:07:19 +0300

    Time: 480 ms (40.17%) | Memory: 73.2 MB (38.43%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -1,32 +1,22 @@
 class Solution {
-    
-    class Fraction {
-        int[] fraction;
-        float value;
-
-        Fraction(int numerator, int denominator) {
-            this.fraction = new int[]{numerator, denominator};
-            this.value = (float) numerator / denominator;
-        }
-    }
 
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        List<Fraction> fractionValues = getFractions(arr);
-        fractionValues.sort(Comparator.comparingDouble(fr -> fr.value));
-        return fractionValues.get(k-1).fraction;
+        List<int[]> fractions = getFractions(arr); 
+        fractions.sort(Comparator.comparingDouble(fraction -> (float) fraction[0]/fraction[1]));
+        return fractions.get(k - 1);
     }
 
-    public  List<Fraction> getFractions (int [] arr) {
-        int n = arr.length; 
-        List<Fraction> fractionValues = new ArrayList<>();
-        for (int i = 0; i < n-1; i++) {
-            int j = i + 1; 
+    public List<int[]> getFractions(int[] arr) {
+        int n = arr.length;
+        List<int[]> fractionValues = new ArrayList<>();
+        for (int i = 0; i < n - 1; i++) {
+            int j = i + 1;
             while (j < n) {
-                fractionValues.add(new Fraction(arr[i], arr[j]));
-                j++; 
+                fractionValues.add(new int[]{arr[i], arr[j]});
+                j++;
             }
         }
-        return fractionValues; 
+        return fractionValues;
     }
 
 }
