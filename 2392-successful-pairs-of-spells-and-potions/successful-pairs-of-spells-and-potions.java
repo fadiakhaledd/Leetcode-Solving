e47f5354281163db1c32387dcb35f18756acde10
Fadia Khaled <fadiakhaledd2@gmail.com>
2024-05-12 18:46:21 +0300

    Time: 42 ms (42.43%) | Memory: 61.1 MB (94.86%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
@@ -4,21 +4,20 @@ class Solution {
         int [] pairs = new int[n];
         Arrays.sort(potions); 
         for (int i = 0; i < n; i++) {
-            long target = success / spells[i] + (success % spells[i] != 0 ? 1 : 0);
-            int index = getIndex(potions, target);
+            long target = (success + spells[i] - 1) / spells[i];
+            int index = getLowerBound(potions, target);
             if (index == -1) pairs[i] = 0; 
             else pairs[i] = potions.length - index;  
         }
         return pairs;
     }
 
-    public int getIndex (int [] potions, long target) {
+    public int getLowerBound (int [] potions, long target) {
         int low = 0, high = potions.length-1; 
         int index = -1; 
         while (low <= high) {
             int mid = low + (high-low)/2; 
              if (target <= potions[mid]) {
-                // Update index but continue searching for earlier occurrences
                 index = mid; 
                 high = mid - 1; 
             } else low = mid + 1; 
