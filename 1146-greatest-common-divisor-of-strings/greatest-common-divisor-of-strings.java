7042486fede0dcf1f794c0b92fcb14c20f0f8001
Fadia Khaled <fadiakhaledd2@gmail.com>
2024-05-22 23:55:54 +0300

    Time: 1 ms (69.98%) | Memory: 42.3 MB (30.19%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -1,11 +1,19 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        return (!(str1 + str2).equals(str2 + str1))? "" : str1.substring(0, gcd(str1.length(), str2.length()));
+
+        if (!(str1 + str2).equals(str2 + str1))
+            return "";
+
+        int gcd = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcd);
     }
 
     public int gcd(int a, int b) {
-        if (b == 0)
-            return a;
-        return gcd(b, a % b);
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }
