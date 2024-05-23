7cd18b640421961a163c17633ce502ccec90e3cb
Fadia Khaled <fadiakhaledd2@gmail.com>
2024-05-23 23:45:25 +0300

    Time: 1 ms (99.60%) | Memory: 43.8 MB (66.00%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,34 +1,26 @@
 class Solution {
     public int compress(char[] chars) {
-        int i = 0, j = 0, k = 0; 
-        int n = chars.length; 
-
+        int i = 0, j = 0, k = 0;
+        int n = chars.length;
         while (i < n) {
-            int count = 0; 
-            char lastChar = chars[i]; 
-            while (j < n) {
-                if (chars[i] != chars[j]){
-                    lastChar = chars[j]; 
-                    break; 
-                }  
-                count++; 
-                j++; 
+            int count = 0;
+            char currentChar = chars[i];
+            while (j < n && chars[i] == chars[j]) {
+                count++;
+                j++;
             }
 
-            if (count > 1) {                
+            chars[k++] = currentChar;
+
+            if (count > 1) {
                 for (char digit : String.valueOf(count).toCharArray()) {
-                    chars[++k] = digit; 
+                    chars[k++] = digit;
                 }
-            } 
-
-            k++; 
-            if (k < n) {
-                chars[k] = lastChar; 
             }
-            
-            i = j; 
+
+            i = j;
         }
 
-        return k; 
+        return k;
     }
 }
