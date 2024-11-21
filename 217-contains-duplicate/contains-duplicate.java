2ae96b865711ef1e2f13e691c9bf9a7f222136e2
Fadia Khaled <fadiakhaledd2@gmail.com>
2024-11-21 22:16:18 +0200

    Time: 7 ms (99.34%) | Memory: 61.4 MB (5.75%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -2,8 +2,7 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> freq = new HashSet<>();
         for (int val : nums) {
-            if (freq.contains(val)) return true; 
-            freq.add(val);
+            if (!freq.add(val)) return true; 
         }
         return false;
     }
